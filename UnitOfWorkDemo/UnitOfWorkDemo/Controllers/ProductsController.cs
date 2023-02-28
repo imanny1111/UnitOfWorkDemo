0905819ffde4308e2c0e494d07f710039a039ca6
Imtiaze Mahmud <77934987+imanny1111@users.noreply.github.com>
2023-02-28 10:35:47 +0800

    init file

---
+++ UnitOfWorkDemo/UnitOfWorkDemo/Controllers/ProductsController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UnitOfWorkDemo.Core.Models;
+using UnitOfWorkDemo.Services.Interfaces;
+
+namespace UnitOfWorkDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProductList()
+        {
+            var productDetailsList = await _productService.GetAllProducts();
+            if (productDetailsList == null)
+            {
+                return NotFound();
+            }
+            return Ok(productDetailsList);
+        }
+
+        [HttpGet("{productId}")]
+        public async Task<IActionResult> GetProductById(int productId)
+        {
+            var productDetails = await _productService.GetProductById(productId);
+
+            if (productDetails != null)
+            {
+                return Ok(productDetails);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+       
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(ProductDetails productDetails)
+        {
+            var isProductCreated = await _productService.CreateProduct(productDetails);
+
+            if (isProductCreated)
+            {
+                return Ok(isProductCreated);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDetails productDetails)
+        {
+            if (productDetails != null)
+            {
+                var isProductCreated = await _productService.UpdateProduct(productDetails);
+                if (isProductCreated)
+                {
+                    return Ok(isProductCreated);
+                }
+                return BadRequest();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+     
+        [HttpDelete("{productId}")]
+        public async Task<IActionResult> DeleteProduct(int productId)
+        {
+            var isProductCreated = await _productService.DeleteProduct(productId);
+
+            if (isProductCreated)
+            {
+                return Ok(isProductCreated);
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+}
